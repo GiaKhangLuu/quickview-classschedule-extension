78003c081deaefffa3a7b53fa67d5d5807e60a72
GiaKhang <giakhang@Thanhs-MacBook-Air-2.local>
2020-09-08 16:21:22 +0700

    This commit is used to redeploy app on heroku

--- Server/process.js
+++ Server/process.js
@@ -3,7 +3,7 @@ var browser = null;
 var page = null;
 
 const Login = async (username, password) => {
-    console.log('Running...');
+    console.log('Run run runnn .....');
     browser = await puppeteer.launch({ headless: false });
     page = await browser.newPage();
     try {
